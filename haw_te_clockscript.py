b0509386c7af6eb07d4fa1ae6a417e18c44ab429
Affensteve <Steffen94Kunz@gmail.com>
2020-04-07 13:47:16 +0200

    Update haw_te_clockscript.py

--- haw_te_clockscript.py
+++ haw_te_clockscript.py
@@ -13,35 +13,15 @@ GPIO.setwarnings(False)
 GRUEN = 2
 GELB = 14
 ROT = 15
+MOND = 26
 TASTER = 18
 
 #Richtung der GPIO-Pins festlegen (IN / OUT)
 GPIO.setup(GRUEN, GPIO.OUT) #GRUEN
 GPIO.setup(GELB, GPIO.OUT) #gelb
 GPIO.setup(ROT, GPIO.OUT) #ROT
+GPIO.setup(MOND, GPIO.OUT) #MOND
 GPIO.setup(TASTER, GPIO.IN) #Taster
-
-#Umschaltung definieren
-def umschalten():
-    #Phase 2
-    GPIO.output(GRUEN, True)
-    GPIO.output(GELB, True)
-    GPIO.output(ROT, False)
-    time.sleep(2)
-    #Phase 3
-    GPIO.output(ROT, True)
-    GPIO.output(GRUEN, False)
-    GPIO.output(GELB, False)
-    time.sleep(4)
-    #Phase 4
-    GPIO.output(GELB, True)
-    GPIO.output(ROT, False)
-    time.sleep(3)
-    #zurueck zu Phase 1
-    GPIO.output(GRUEN, True)
-    GPIO.output(GELB, False)
-
-    
 
 def ledtest():
    while True:
@@ -49,16 +29,16 @@ def ledtest():
 	tasterStatus = GPIO.input(TASTER)
 	
     	if (tasterStatus):
-   	print("Number of cpu : ", multiprocessing.cpu_count())
-   	for i in range(1, 55):
-		GPIO.output(GRUEN, False)
-		GPIO.output(GELB, True)
-		GPIO.output(ROT, True)
-		time.sleep(0.5)
-		GPIO.output(GRUEN, True)
-		GPIO.output(GELB, False)
-		GPIO.output(ROT, False)
-		time.sleep(0.5)
+		print("Number of cpu : ", multiprocessing.cpu_count())
+		for i in range(1, 10):
+			GPIO.output(GRUEN, False)
+			GPIO.output(GELB, True)
+			GPIO.output(ROT, True)
+			time.sleep(0.5)
+			GPIO.output(GRUEN, True)
+			GPIO.output(GELB, False)
+			GPIO.output(ROT, False)
+			time.sleep(0.5)
 
 def modulateSeconds():
    while True:
@@ -66,6 +46,13 @@ def modulateSeconds():
       time.sleep(0.5)
       GPIO.output(GRUEN, True)
       time.sleep(0.5)
+	
+def moonphaseIndicator():
+   while True:
+	sleep(25)
+	for j in range(1,121):
+		GPIO.output(MOND, True)
+	GPIO.output(MOND, False)
 
 
 if __name__ == '__main__':
@@ -73,19 +60,19 @@ if __name__ == '__main__':
     GPIO.output(GRUEN, True)
     GPIO.output(GELB, False)
     GPIO.output(ROT, False)
-
-
-    #Status des Tasters einlesen
-    #tasterStatus = GPIO.input(TASTER)
-    #if (tasterStatus):
-        #ledtest()
+    GPIO.output(MOND, False)
 
     thread = multiprocessing.Process(target=modulateSeconds)
-    thread.daemon = True
+    thread.daemon = False
     
     thread2 = multiprocessing.Process(target=ledtest)
-    thread2.daemon = Frue
+    thread2.daemon = False
+	
+    threadMoon = multiprocessing.Process(target=moonphaseIndicator)
+    threadMoon.daemon = False
 
     thread.start()
     time.sleep(1)
     thread2.start()
+    time.sleep(1)
+    threadMoon.start()
