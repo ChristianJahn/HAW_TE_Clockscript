63a4b4da835ae950969b91511d87d1a4c2671bdd
Affensteve <Steffen94Kunz@gmail.com>
2020-04-07 13:09:42 +0200

    Update haw_te_clockscript.py

--- haw_te_clockscript.py
+++ haw_te_clockscript.py
@@ -1,6 +1,7 @@
 #bliotheken einbinden
 import RPi.GPIO as GPIO
 import time
+import multiprocessing
 
 #GPIO Modus (BOARD / BCM)
 GPIO.setmode(GPIO.BCM)
@@ -41,24 +42,50 @@ def umschalten():
     GPIO.output(GELB, False)
 
     
-def ledtest()
-for i in range(1, 5):
-	GPIO.output(GRUEN, True)
-    GPIO.output(GELB, True)
-    GPIO.output(ROT, True)
-    time.sleep(1)
-    print(time.ctime)
-    
-#Endlosschleife
-while True:
+
+def ledtest():
+   while True:
+	#Status des Tasters einlesen
+	tasterStatus = GPIO.input(TASTER)
+	
+    	if (tasterStatus):
+   	print("Number of cpu : ", multiprocessing.cpu_count())
+   	for i in range(1, 55):
+		GPIO.output(GRUEN, False)
+		GPIO.output(GELB, True)
+		GPIO.output(ROT, True)
+		time.sleep(0.5)
+		GPIO.output(GRUEN, True)
+		GPIO.output(GELB, False)
+		GPIO.output(ROT, False)
+		time.sleep(0.5)
+
+def modulateSeconds():
+   while True:
+      GPIO.output(GRUEN, False)
+      time.sleep(0.5)
+      GPIO.output(GRUEN, True)
+      time.sleep(0.5)
+
+
+if __name__ == '__main__':
     #Phase 1
     GPIO.output(GRUEN, True)
     GPIO.output(GELB, False)
     GPIO.output(ROT, False)
-    
-    
+
 
     #Status des Tasters einlesen
-    tasterStatus = GPIO.input(TASTER)
-    if (tasterStatus):
-        ledtest()
+    #tasterStatus = GPIO.input(TASTER)
+    #if (tasterStatus):
+        #ledtest()
+
+    thread = multiprocessing.Process(target=modulateSeconds)
+    thread.daemon = True
+    
+    thread2 = multiprocessing.Process(target=ledtest)
+    thread2.daemon = Frue
+
+    thread.start()
+    time.sleep(1)
+    thread2.start()
