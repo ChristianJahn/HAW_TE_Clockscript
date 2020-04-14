b95217f363550569186ba252f3eb2ef6815f639e
Affensteve <Steffen94Kunz@gmail.com>
2020-04-14 14:10:47 +0200

    Final Version of haw clock
    runs each hour 2 seconds to run the moon phases

--- haw_te_clockscript.py
+++ haw_te_clockscript.py
@@ -15,6 +15,9 @@ GELB = 14
 ROT = 15
 MOND = 26
 TASTER = 18
+ZYKLUS = 3595
+RANGE = 6
+#entweder 5 Sek in einer Std(3600 Seks) oder am Tag(86400 Seks) 120 Sek
 
 #Richtung der GPIO-Pins festlegen (IN / OUT)
 GPIO.setup(GRUEN, GPIO.OUT) #GRUEN
@@ -25,20 +28,19 @@ GPIO.setup(TASTER, GPIO.IN) #Taster
 
 def ledtest():
    while True:
-	#Status des Tasters einlesen
-	tasterStatus = GPIO.input(TASTER)
-	
-    	if (tasterStatus):
-		print("Number of cpu : ", multiprocessing.cpu_count())
-		for i in range(1, 10):
-			GPIO.output(GRUEN, False)
-			GPIO.output(GELB, True)
-			GPIO.output(ROT, True)
-			time.sleep(0.25)
-			GPIO.output(GRUEN, True)
-			GPIO.output(GELB, False)
-			GPIO.output(ROT, False)
-			time.sleep(0.25)
+        #Status des Tasters einlesen
+        tasterStatus = GPIO.input(TASTER)
+
+        if (tasterStatus):
+                for i in range(1, 10):
+                        GPIO.output(GRUEN, False)
+                        GPIO.output(GELB, True)
+                        GPIO.output(ROT, True)
+                        time.sleep(0.25)
+                        GPIO.output(GRUEN, True)
+                        GPIO.output(GELB, False)
+                        GPIO.output(ROT, False)
+                        time.sleep(0.25)
 
 def modulateSeconds():
    while True:
@@ -46,15 +48,15 @@ def modulateSeconds():
       time.sleep(0.5)
       GPIO.output(GRUEN, True)
       time.sleep(0.5)
-	
+
 def moonphaseIndicator():
    while True:
-	time.sleep(34439)
-	for j in range(1,121):
-		GPIO.output(MOND, True)
-		time.sleep(1)
-		
-	GPIO.output(MOND, False)
+        time.sleep(ZYKLUS)
+        for j in range(1,RANGE):
+                GPIO.output(MOND, True)
+                time.sleep(1)
+
+        GPIO.output(MOND, False)
 
 
 if __name__ == '__main__':
@@ -66,10 +68,10 @@ if __name__ == '__main__':
 
     thread = multiprocessing.Process(target=modulateSeconds)
     thread.daemon = False
-    
+
     thread2 = multiprocessing.Process(target=ledtest)
     thread2.daemon = False
-	
+
     threadMoon = multiprocessing.Process(target=moonphaseIndicator)
     threadMoon.daemon = False
 
